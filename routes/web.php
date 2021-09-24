f7af4e2574ad597d9f11e4752e0298fdd99daeba
SIOM-APP <72476800+Hector-Are@users.noreply.github.com>
2021-09-24 01:54:03 -0500

    Act

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/users', [UserController::class, 'show']);
+
+Route::get('/user/form/{id?}', [UserController::class, 'form'])->name('user.form');
+
+Route::post('/user/save', [UserController::class, 'save'])->name('user.save');
+
